5d85a5ffb86c56cff13be23e42dbd737231f94cb
Letícia Duarte <leticiabsduarte@gmail.com>
2022-06-21 20:57:25 -0300

    feat: added counter

---
+++ while/Count.java
@@ -0,0 +1,10 @@
+public class Count {
+    public static void main(String[] args) {
+        
+        int x = 0;
+        while(x<11) {
+            System.out.println("contador: " +x);
+            x++;
+        }
+    }
+}
