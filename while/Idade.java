2cbec534342dd7a80a65d0f269870c46b5c25a22
Letícia Duarte <leticiabsduarte@gmail.com>
2022-06-24 14:55:06 -0300

    feat: loop on age system

--- while/Idade.java
+++ while/Idade.java
@@ -6,14 +6,15 @@ public class Idade {
 
         int idade;
 
-        System.out.println("Digite sua idade:");
-        idade = leitor.nextInt();
-
-        if(idade<18) {
-            System.out.println("Você não pode acessar este programa");
-        } else {
-            System.out.println("Tudo certo! Entrando no sistema...");
-        }
+        do {
+            System.out.println("Digite sua idade:");
+            idade = leitor.nextInt(); 
+                if (idade <18) {
+                System.out.println("Você não pode acessar este programa");
+                } else {
+                System.out.println("Tudo certo! Entrando no sistema...");   
+                }
+            } while (idade<18);
         leitor.close();
     }
 }
