2567ef2372459739415f4e2945b5e6468f91c03a
Letícia Duarte <leticiabsduarte@gmail.com>
2022-06-30 00:54:37 -0300

    feat: inverter done

--- invertas/Inverte.java
+++ invertas/Inverte.java
@@ -1,19 +1,24 @@
 /* Receber 5 números inteiros de um array A e inverter a ordem dos valores equivalentes em um array B. */
-
-import java.util.Scanner;
-
 public class Inverte {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-
+       
         int i = 0;
+    
         int[] numero = new int[5];
+        int[] invertida = new int[5];
+
+        numero [0] = 1;
+        numero [1] = 2;
+        numero [2] = 3;
+        numero [3] = 4;
+        numero [4] = 5;
 
         for (i=0; i < numero.length; i++){
-            System.out.println("Digite 5 números inteiros:");
-            numero[i] = leitor.nextInt();
+            invertida[4 - i] = numero[i];
         }
         
-
+        for (i=0; i < numero.length; i++){
+            System.out.println(numero[i] + " " + invertida[i]);
+        }    
     }
 }
