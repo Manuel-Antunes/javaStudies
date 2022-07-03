2abcaeac3320c900266cc7491935a05ec6d7bd55
Letícia Duarte <leticiabsduarte@gmail.com>
2022-07-02 23:19:37 -0300

    feat: bubble sort

--- crescealto/Crescente.java
+++ crescealto/Crescente.java
@@ -6,27 +6,27 @@ public class Crescente {
         Scanner leitor = new Scanner(System.in);
 
         int i = 0;
-        int j = 1;
-        int maior = 0;
+        int j = 0;
+        int aux = 0;
 
         int[] numeros = new int[5];
         for (i=0; i < numeros.length; i++){
             System.out.println("Digite 5 números:");
             numeros[i] = leitor.nextInt();
         }
-        leitor.close();
 
-        int[] crescente = new int[5];        
-        for (i=0; i < numeros.length; i++){
-            for (j = 1; j < numeros.length; j++){
-                if (numeros[j]>numeros[i]){
-                crescente[i] = maior; 
-                }  
+        for(i = 0; i<5; i++){
+            for(j = 0; j<4; j++){
+                if(numeros[j] > numeros[j + 1]){
+                    aux = numeros[j];
+                    numeros[j] = numeros[j+1];
+                    numeros[j+1] = aux;
+                }
             }
         }
 
         for(i=0; i<numeros.length;i++){
-            System.out.println(numeros[i] + " " + crescente[i]);
+            System.out.println(numeros[i] + " " + numeros[j]);
         }
     }
 }
