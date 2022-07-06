406ddae80872b91b8cec35ce5199a3b82e36bf6e
Manuel Nascimento <57446204+Manuel-Antunes@users.noreply.github.com>
2022-07-05 22:33:18 -0300

    Update Crescente.java

--- crescealto/Crescente.java
+++ crescealto/Crescente.java
@@ -4,29 +4,22 @@ import java.util.Scanner;
 public class Crescente {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-
-        int i = 0;
-        int j = 0;
-        int aux = 0;
-
         int[] numeros = new int[5];
         for (i=0; i < numeros.length; i++){
             System.out.println("Digite 5 números:");
             numeros[i] = leitor.nextInt();
         }
-
         int[] crescente = new int[5];
-
         for(i = 0; i<5; i++){
             crescente[i] = numeros[i];
         }
 
-        for(i = 0; i<5; i++){
-            for(j = 0; j<4; j++){
-                if(crescente[j] > crescente[j + 1]){
-                    aux = crescente[j];
-                    crescente[j] = crescente[j+1];
-                    crescente[j+1] = aux;
+        for(int i = 0; i <5; i++){
+            for(int j = i+1; j <5; j++){
+                if(crescente[i]> crescente[j + 1]){
+                    int aux = crescente[i];
+                    crescente[i] = crescente[j];
+                    crescente[j] = aux;
                 }
             }
         }
